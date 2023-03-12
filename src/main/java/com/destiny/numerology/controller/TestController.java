81c5d62dd52b706f386f969bd725969b21cba521
rundongzhichu <2298812879@qq.com>
2023-03-12 16:19:11 +0800

    write a test controller case

--- src/main/java/com/destiny/numerology/controller/TestController.java
+++ src/main/java/com/destiny/numerology/controller/TestController.java
@@ -1,5 +1,19 @@
 package com.destiny.numerology.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping(value = "/destiny/test")
 public class TestController {
+
+    @GetMapping(value = "/hello/{name}")
+    @ResponseBody
+    public String hello(@PathVariable(value = "name") String name){
+        return "Hello " + name;
+    }
+
 }
